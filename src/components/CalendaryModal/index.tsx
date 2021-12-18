f25d360faa681226fd669ba9ea965813167491f6
LuizSouz404 <luiz.souz404@gmail.com>
2021-12-18 15:16:04 -0300

    Feat: create calendary page

--- src/components/CalendaryModal/index.tsx
+++ src/components/CalendaryModal/index.tsx
@@ -1,10 +1,38 @@
 import Modal from 'react-modal';
 import Calendar from 'react-calendar';  
+import styles from './styles.module.scss';
+import {CgChevronLeft, CgChevronRight} from 'react-icons/cg';
 
 export function CalendaryModal() {
   return (
-    <Modal isOpen={true}>
-      <Calendar className='react-calendar' />
+    <Modal
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content-calendar" 
+      isOpen={true}
+    >
+      <div className={styles.container}>
+        <Calendar 
+        tileContent={""}
+          locale='pt-BR' 
+          prevLabel={<CgChevronLeft /> }  
+          nextLabel={<CgChevronRight /> }
+          next2Label={""}  
+          prev2Label={""}  
+          tileClassName={styles.tile} 
+          className='react-calendar' 
+        />
+
+        <div className={styles.content}>
+          <div className={styles.todo}>
+            <h2>hello world</h2>
+            <span>5/5</span>
+          </div>
+          <div className={styles.todo}>
+            <h2>hello world</h2>
+            <span>2/5</span>
+          </div>  
+        </div>
+      </div>
 
     </Modal>
   )
