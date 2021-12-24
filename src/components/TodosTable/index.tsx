971b0d5420861e4a0f8d29404b81c5841352ea29
LuizSouz404 <luiz.souz404@gmail.com>
2021-12-24 12:00:26 -0300

    Chore: Connect TodosTable with backend API

--- src/components/TodosTable/index.tsx
+++ src/components/TodosTable/index.tsx
@@ -1,15 +1,26 @@
 import styles from './styles.module.scss';
+import { isEqual, getISODay } from 'date-fns'
 
-import { useTodos } from '../../hooks/useToDo';
+import { ITodoCategory, useTodos } from '../../hooks/useToDo';
 import { TodoList } from '../TodoList';
+import { useEffect, useState } from 'react';
 
 export function TodosTable() {
   const {todos} = useTodos();
+  const [dayTodo, setDayTodo] = useState<ITodoCategory[]>([]);
+
+  useEffect(() => {
+    const newArray = todos;
+
+    const currentDateTodos = newArray.filter(todo => isEqual(new Date(todo.createdAt).setHours(0, 0, 0, 0), new Date().setHours(0, 0, 0, 0)));
+
+    setDayTodo(currentDateTodos);
+  }, [todos])
 
   return (
     <>
       <div className={styles.container}>
-        {todos.map(todo => (
+        {dayTodo.map(todo => (
           <TodoList key={todo.id} {...todo}/>
         ))}
       </div>
