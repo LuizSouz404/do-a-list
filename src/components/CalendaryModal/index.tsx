004508f1e5f64a19bee6c0a4dc462df5a3df14bc
LuizSouz404 <luiz.souz404@gmail.com>
2021-12-25 18:39:45 -0300

    Chore: refact styles pattern

--- src/components/CalendaryModal/index.tsx
+++ src/components/CalendaryModal/index.tsx
@@ -1,8 +1,9 @@
-import Calendar from 'react-calendar';  
+import Calendar from 'react-calendar';
 import styles from './styles.module.scss';
 import {CgChevronLeft, CgChevronRight} from 'react-icons/cg';
 import { useEffect, useState } from 'react';
 import { ITodoCategory, useTodos } from '../../hooks/useToDo';
+import { isEqual } from 'date-fns';
 
 export function CalendaryModal() {
   const { todos } = useTodos();
@@ -12,25 +13,21 @@ export function CalendaryModal() {
   useEffect(() => {
     const newArray = todos;
 
-    const currentDateTodos = newArray.filter(todo => 
-      `${new Date(todo.createdAt.getDate(), todo.createdAt.getMonth(), todo.createdAt.getFullYear())}`
-      === 
-      `${new Date(date.getDate(), date.getMonth(), date.getFullYear())}`
-    );
+    const currentDateTodos = newArray.filter(todo => isEqual(new Date(todo.createdAt).setHours(0, 0, 0, 0), new Date(date).setHours(0, 0, 0, 0)));
 
     setDayTodo(currentDateTodos);
-  }, [date])
+  }, [date, todos])
 
-  return (    
+  return (
     <div className={styles.container}>
-      <Calendar 
+      <Calendar
       tileContent={""}
-        locale='pt-BR' 
-        prevLabel={<CgChevronLeft /> }  
+        locale='pt-BR'
+        prevLabel={<CgChevronLeft /> }
         nextLabel={<CgChevronRight /> }
-        next2Label={""}  
-        prev2Label={""}  
-        tileClassName={styles.tile} 
+        next2Label={""}
+        prev2Label={""}
+        tileClassName={styles.tile}
         className={styles.reactCalendar}
         onClickDay={(value) => setDate(value)}
         value={date}
@@ -38,9 +35,9 @@ export function CalendaryModal() {
 
       <div className={styles.content}>
         {dayTodo.map(list => (
-          <div 
-            className={styles.todo} 
-            key={list.id} 
+          <div
+            className={styles.todo}
+            key={list.id}
             style={list.color === '#fefeff' ? {color: "#000", fill: "#000", background: list.color} : {color: "#fff", fill: "#fff", background: list.color}}
           >
             <h2>{list.title}</h2>
