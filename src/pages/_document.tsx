2764053c245206feb6d52da226566c58f200c863
LuizSouz404 <luiz.souz404@gmail.com>
2021-12-26 18:49:57 -0300

    Chore: att _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
 
           <link rel="shortcut icon" href="/logo.svg" type="image/svg" />
         </Head>
