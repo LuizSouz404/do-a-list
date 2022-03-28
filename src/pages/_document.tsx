3d22a4cf26cc2cef34367af307c1ff3a8b377a4e
Luiz Souza <79121809+LuizSouz404@users.noreply.github.com>
2022-03-28 18:01:26 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,14 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
 
           <link rel="shortcut icon" href="/logo.svg" type="image/svg" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-X4KH2BRZEY"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X4KH2BRZEY');
+          </script>
         </Head>
         <body>
           <Main />
